a9772cebd23a0ff67167eea6430d1f41f5b30807
neria34 <neria21214@gmail.com>
2019-09-22 17:45:53 +0900

    문명6 인용구추가 및 시간별 출력 변경

--- civbot.py
+++ civbot.py
@@ -1,9 +1,10 @@
 from mastodon import Mastodon
+from datetime import datetime
 import json
 import random as rand
 
 
-with open("../key/mastodon.json", encoding="utf-8") as key:
+with open("./key/key.json", encoding="utf-8") as key:
     token: dict = json.load(key)
 
 Civbot = Mastodon(
@@ -11,14 +12,29 @@ Civbot = Mastodon(
     api_base_url='https://mastodon.social'
 )
 
-with open("./quotes/quotes.json", encoding="utf-8") as fp:
-    quotes: dict = json.load(fp)
+
+time = datetime.today().minute
+if (time >= 40):
+    with open("./quotes/civ5quotes.json", encoding="utf-8") as fp:
+        quotes: dict = json.load(fp)
+elif (time >= 20):
+    with open("./quotes/civBEquotes.json", encoding="utf-8") as fp:
+        quotes: dict = json.load(fp)
+else:
+    with open("./quotes/civ6quotes.json", encoding="utf-8") as fp:
+        quotes: dict = json.load(fp)
+
 
 i = rand.randint(0, len(quotes)-1)
 
-post = "["+quotes[i]["name"]+"]\n" + \
-    quotes[i]["quote"]+"\n- " + \
-    quotes[i]["author"]+"\n#"+quotes[i]["version"]
+if quotes[i]["author"] is None:
+    post = "["+quotes[i]["name"]+"]\n" + \
+        quotes[i]["quote"]+"\n#"+quotes[i]["version"]
+else:
+    post = "["+quotes[i]["name"]+"]\n" + \
+        quotes[i]["quote"]+"\n- " + \
+        quotes[i]["author"]+"\n#"+quotes[i]["version"]
+
 
 if "image" in quotes[i]:
     media = Civbot.media_post(quotes[i]["image"])
