280d200026115bc4336a455c3d8aa45180ff56b2
vallisneria <vallisneria@gmail.com>
2019-11-10 11:32:33 +0900

    문명 5와 6의 인용구 분리

--- civbot.py
+++ civbot.py
@@ -3,6 +3,7 @@ from mastodon import Mastodon
 import random as rand
 import json
 from key import MASTODON_ACCESS_TOKEN
+from dir import DIRECTORY
 
 
 # 메인 코드 부분
@@ -11,7 +12,7 @@ Civbot = Mastodon(
     api_base_url='https://mastodon.social'
 )
 
-with open("/home/vallisneria/mastodon-civ-quote-bot-kr/quotes/civquotes.json", encoding="utf-8") as json_file:
+with open(DIRECTORY + "/quotes/civ5quotes.json", encoding="utf-8") as json_file:
     quotes = json.load(json_file)
 
 i = rand.randint(0, len(quotes)-1)
@@ -26,7 +27,7 @@ else:
 
 
 if "image" in quotes[i]:
-    medialocation = "/home/vallisneria/mastodon-civ-quote-bot/" + quotes[i]["image"]
+    medialocation = DIRECTORY + quotes[i]["image"]
     media = Civbot.media_post(medialocation)
     Civbot.status_post(post, media_ids=media, visibility="unlisted")
 else:
