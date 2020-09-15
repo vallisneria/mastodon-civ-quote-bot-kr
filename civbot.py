e41aa46d98cdd61b654581937752f2255dc9abae
vallisneria <moogi3469@gmail.com>
2020-09-15 19:08:08 +0900

    해당 코드를 실행할 플랫폼을 Github Action으로 변경

--- civbot.py
+++ civbot.py
@@ -2,17 +2,15 @@
 from mastodon import Mastodon
 import random as rand
 import json
-from key import MASTODON_ACCESS_TOKEN
 import os
-DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
 # 메인 코드 부분
 Civbot = Mastodon(
-    access_token=MASTODON_ACCESS_TOKEN,
-    api_base_url='https://mastodon.social'
+    access_token=os.environ["MASTODON_ACCESS_TOKEN"],
+    api_base_url=os.environ["MASTODON_BASE_URL"]
 )
 
-with open(DIRECTORY + "/quotes/civ5quotes.json", encoding="utf-8") as json_file:
+with open("./quotes/civ5quotes.json", encoding="utf-8") as json_file:
     quotes = json.load(json_file)
 
 i = rand.choice(quotes)
@@ -27,7 +25,7 @@ else:
 
 
 if "image" in i:
-    medialocation = DIRECTORY + i["image"]
+    medialocation = "." + i["image"]
     media = Civbot.media_post(medialocation)
     Civbot.status_post(post, media_ids=media, visibility="unlisted")
 else:
